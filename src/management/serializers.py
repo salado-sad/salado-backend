65a7a636da6ba69abf14c1c51ebad242c0014a30
unknown <amirhoseinakbari202@gmail.com>
2025-02-08 15:30:31 +0330

    package api fixed

--- src/management/serializers.py
+++ src/management/serializers.py
@@ -1,22 +1,7 @@
 from rest_framework import serializers
-from .models import Package, PackageProduct
-from vendors.models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ["id", "name", "image"]
-
-class PackageProductSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-
-    class Meta:
-        model = PackageProduct
-        fields = ["product", "quantity"]
+from .models import Package
 
 class PackageSerializer(serializers.ModelSerializer):
-    products = PackageProductSerializer(source="packageproduct_set", many=True, read_only=True)
-
     class Meta:
         model = Package
-        fields = ["id", "name", "price", "description", "image", "products"]
+        fields = "__all__"
--- src/management/views.py
+++ src/management/views.py
@@ -1,28 +1,25 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Package, PackageProduct
-from .serializers import PackageSerializer
-from vendors.models import Product
 from rest_framework import status
+from .models import Package
+from .serializers import PackageSerializer
 
 class PackageViewSet(viewsets.ModelViewSet):
     queryset = Package.objects.all()
     serializer_class = PackageSerializer
 
     def create(self, request, *args, **kwargs):
+        """
+        Create a package with product names and quantities directly in JSONField.
+        """
         data = request.data
         package = Package.objects.create(
             name=data["name"],
             price=data["price"],
             description=data.get("description", ""),
             image=data.get("image", None),
+            products=data["products"]  # List of product details
         )
-
-        for product in data.get("products", []):
-            product_instance = Product.objects.get(id=product["product_id"])
-            PackageProduct.objects.create(
-                package=package, product=product_instance, quantity=product["quantity"]
-            )
 
         serializer = PackageSerializer(package)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
