a4a04e8a18f89a13d1fdf1de6c88f69d5d362565
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2025-03-05 23:15:36 +0330

    Add logout url

--- src/auth_system/urls.py
+++ src/auth_system/urls.py
@@ -11,11 +11,12 @@
 #
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, get_user_profile, CustomTokenObtainPairView
+from .views import RegisterView, get_user_profile, CustomTokenObtainPairView, logout_view
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', get_user_profile, name='profile'),
+    path('logout', logout_view, name='logout')
 ]
