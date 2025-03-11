cb132cf75d69e3bf6248227bab0ebd22993cb04e
EmadEJ <s.emad.emamjomeh@gmail.com>
2025-03-11 13:42:30 +0330

    bomb

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -1,7 +1,7 @@
 # cart/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CartItemViewSet, CartViewSet, PurchaseListView, PublicCartItemViewSet, PurchaseListView
+from .views import CartItemViewSet, CartViewSet, PurchaseListView, PublicCartItemViewSet, PublicPurchaseListView
 
 router = DefaultRouter()
 router.register(r'items', CartItemViewSet, basename='cartitem')
